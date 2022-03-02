fda36fa19d77ef511c0a28162bd0303de7b859b6
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2022-03-02 14:45:23 -0500

    working on plantier

---
+++ plantier/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function header() {
+  return (
+    <div>
+        <h1>this is Header</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi quidem veniam sequi in recusandae ratione inventore earum! In, labore quam.</p>
+    </div>
+  )
+}
---
+++ plantier/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+import styles from '../styles/Home.module.css'
+import Header from '../components/header'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Header/>
+    </div>
+  )
+}
